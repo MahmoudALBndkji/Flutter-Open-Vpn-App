0353b84b7281cd33ba35eea4e8500d1679fb10af
MahmoudALBndkji <mahmoudalbndkji@gmail.com>
2025-02-26 18:04:04 +0300

    Add Connection Vpn To Home Controller

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:vpn_basic_project/helper/app_helper.dart';
+import 'package:vpn_basic_project/helper/vpn-engine/vpn_engine.dart';
+import 'package:vpn_basic_project/models/vpn_configuration_model.dart';
 import 'package:vpn_basic_project/models/vpn_info_model.dart';
 
 class HomeController extends GetxController {
@@ -7,4 +12,26 @@ class HomeController extends GetxController {
   // obs => Turn anything into something observable
   // When Use [Rx] Should Use [obs]
   final Rx<VpnInfoModel> vpnInfo = AppHelper.vpnInfoObejct.obs;
+  final vpnConnectionState = VpnEngine.vpnDisconnectedNow.obs;
+
+  void connectionToVpnNow() async {
+    if (vpnInfo.value.base64OpenVpnConfigurationData.isEmpty) {
+      Get.snackbar(
+          "Country / Location", "Please Select Country / Location First");
+      return;
+    }
+    // Disconnected
+    if (vpnConnectionState.value == VpnEngine.vpnDisconnectedNow) {
+      final dataConfigVpn =
+          Base64Decoder().convert(vpnInfo.value.base64OpenVpnConfigurationData);
+      final configuration = Utf8Decoder().convert(dataConfigVpn);
+      final vpnConfiguration = VpnConfigurationModel(
+        username: "vpn",
+        password: "vpn",
+        countryName: vpnInfo.value.countryLongName,
+        config: configuration,
+      );
+      await VpnEngine.startVpnNow(vpnConfiguration);
+    }
+  }
 }
