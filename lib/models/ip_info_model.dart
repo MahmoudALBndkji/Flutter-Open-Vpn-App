38131659485d468f3619429551b21d97f81907f5
MahmoudALBndkji <mahmoudalbndkji@gmail.com>
2025-03-05 14:50:09 +0300

    Add Ip Info Model

---
+++ lib/models/ip_info_model.dart
@@ -0,0 +1,31 @@
+class IpInfoModel {
+  late final String countryName,
+      regionName,
+      cityName,
+      zipCode,
+      timeZone,
+      internetServiceProvider,
+      query;
+
+  IpInfoModel({
+    required this.countryName,
+    required this.regionName,
+    required this.cityName,
+    required this.zipCode,
+    required this.timeZone,
+    required this.internetServiceProvider,
+    required this.query,
+  });
+
+  factory IpInfoModel.fromJson(Map<String, dynamic> json) {
+    return IpInfoModel(
+      countryName: json['country'] ?? '',
+      regionName: json['region'] ?? '',
+      cityName: json['city'] ?? '',
+      zipCode: json['zip'] ?? '',
+      timeZone: json['timezone'] ?? '',
+      internetServiceProvider: json['isp'] ?? '',
+      query: json['query'] ?? '',
+    );
+  }
+}
